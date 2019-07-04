bf75838a1b807f5528e76a9662f8341aa5e6a3b3
Youness HAKIM <younesshakim19@gmail.com>
2019-07-04 13:04:31 +0100

    main page and login page front end done

---
+++ index.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <title>My APP</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
+    <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+      }
+    }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+<body>
+  <nav id="nav">
+    <ul>
+      <li><a href="#" class="active">Nearby Shops</a></li>
+      <li><a href="#">Prefered Shops</a></li>
+    </ul>
+  </nav>
+  <div id="container">
+    
+    <div class="shop">
+      <span class="name"><p>Shop name 1</p></span>
+      <span class="image"><img src="img/image.png"></span>
+      <span class="dislikebtn">Dislike</span><span class="likebtn">Like</span>
+    </div>
+    <div class="shop">
+      <span class="name"><p>Shop name 2</p></span>
+      <span class="image"><img src="img/image.png"></span>
+      <span class="dislikebtn">Dislike</span><span class="likebtn">Like</span>
+    </div>
+    <div class="shop">
+      <span class="name"><p>Shop name 3</p></span>
+      <span class="image"><img src="img/image.png"></span>
+      <span class="dislikebtn">Dislike</span><span class="likebtn">Like</span>
+    </div>
+    <div class="shop">
+      <span class="name"><p>Shop name 4</p></span>
+      <span class="image"><img src="img/image.png"></span>
+      <span class="dislikebtn">Dislike</span><span class="likebtn">Like</span>
+    </div>
+    <div class="shop">
+      <span class="name"><p>Shop name 5</p></span>
+      <span class="image"><img src="img/image.png"></span>
+      <span class="dislikebtn">Dislike</span><span class="likebtn">Like</span>
+    </div>
+    <div class="shop">
+      <span class="name"><p>Shop name 6</p></span>
+      <span class="image"><img src="img/image.png"></span>
+      <span class="dislikebtn">Dislike</span><span class="likebtn">Like</span>
+    </div>
+    <div class="shop">
+      <span class="name"><p>Shop name 7</p></span>
+      <span class="image"><img src="img/image.png"></span>
+      <span class="dislikebtn">Dislike</span><span class="likebtn">Like</span>
+    </div>
+    <div class="shop">
+      <span class="name"><p>Shop name 8</p></span>
+      <span class="image"><img src="img/image.png"></span>
+      <span class="dislikebtn">Dislike</span><span class="likebtn">Like</span>
+    </div>
+    <div class="shop">
+      <span class="name"><p>Shop name 9</p></span>
+      <span class="image"><img src="img/image.png"></span>
+      <span class="dislikebtn">Dislike</span><span class="likebtn">Like</span>
+    </div>
+    <div class="shop">
+      <span class="name"><p>Shop name 10</p></span>
+      <span class="image"><img src="img/image.png"></span>
+      <span class="dislikebtn">Dislike</span><span class="likebtn">Like</span>
+    </div>
+    <div class="shop">
+      <span class="name"><p>Shop name 11</p></span>
+      <span class="image"><img src="img/image.png"></span>
+      <span class="dislikebtn">Dislike</span><span class="likebtn">Like</span>
+    </div>
+    <div class="shop">
+      <span class="name"><p>Shop name 12</p></span>
+      <span class="image"><img src="img/image.png"></span>
+      <span class="dislikebtn">Dislike</span><span class="likebtn">Like</span>
+    </div>
+  </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,46 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <title>My APP</title>
+
+    <!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+<form class="form-signin">
+	<h1 class="h3 mb-3 font-weight-normal">Sign in</h1>
+	<label for="inputEmail" class="sr-only">Email address</label>
+	<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+	<label for="inputPassword" class="sr-only">Password</label>
+	<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+	<div class="checkbox mb-3">
+	</div>
+	<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+	<label><a href="">Sign Up</a></label>
+</form>
+	
+</body>
+</html>
